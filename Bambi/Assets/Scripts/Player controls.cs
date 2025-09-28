c0d3b28391e1808401b551eaab0700a1a37ebc57
achhi2010 <138145642+achhi2010@users.noreply.github.com>
2025-09-28 19:07:48 +0530

    Adding controls to the space ship and limiting its movement

--- Bambi/Assets/Scripts/Player controls.cs
+++ Bambi/Assets/Scripts/Player controls.cs
@@ -1,17 +1,24 @@
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SocialPlatforms;
 
 public class Playercontrols : MonoBehaviour
 {
+    [SerializeField] float Steerspeed;
+    [SerializeField] float Xrange;
+    [SerializeField] float Yrange;
     void Update()
     {
-        float Xoffset = .5f;
         float horizontalSteer = Input.GetAxis("Horizontal");
         float verticalSteer = Input.GetAxis("Vertical");
-        float NewXpos = transform.localPosition.x + Xoffset;
-    
+        float Xoffset = horizontalSteer * Time.deltaTime * Steerspeed;
+        float Yoffset = verticalSteer * Time.deltaTime * Steerspeed;
+        float OldXpos = transform.localPosition.x + Xoffset;
+        float OldYpos = transform.localPosition.y + Yoffset;
+        float ClampedXpos = Mathf.Clamp(OldXpos, -Xrange, Xrange);
+        float ClampedYpos = Mathf.Clamp(OldYpos, -Yrange, Yrange);    
         transform.localPosition = new Vector3
-        (NewXpos, transform.localPosition.y, transform.localPosition.z);
+        (ClampedXpos, ClampedYpos);
     }
 }
